1faea489aae26f1c65fb5f4db2d5e13a0ca945ad
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-09 13:57:57 +0300

    change AddUser component

--- my-form/src/App.js
+++ my-form/src/App.js
@@ -1,8 +1,11 @@
+import AddUser from "./components/Users/AddUser";
+
+
 
 function App() {
   return (
     <div>
-      
+      <AddUser/>
     </div>
   );
 }
--- my-form/src/components/Users/AddUser.js
+++ my-form/src/components/Users/AddUser.js
@@ -3,7 +3,10 @@ import React from "react";
 const AddUser = props => {
     return (
         <form>
-            <label htmlFor="Username">Username</label>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+            <label htmlFor="Age">Age</label>
+            <input type="text" id="age" />
         </form>
     )
 }
