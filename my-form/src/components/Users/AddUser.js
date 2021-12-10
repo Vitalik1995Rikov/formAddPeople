cf3632ac2fac67a6775b6ac5cff873aba21fef65
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-11 01:23:11 +0300

    added onChange event

--- my-form/src/components/Users/AddUser.js
+++ my-form/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
@@ -6,15 +6,21 @@ import classes from './AddUser.module.css';
 
 const AddUser = props => {
 
+    const [enteredUserName, setEnteredUserName] = useState('');
+
     function addUserHandler(event) {
         event.preventDefault();
+    }
+
+    function userNameChangeHandler(event) {
+        setEnteredUserName(event.target.value);
     }
 
     return (
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" onChange={userNameChangeHandler}/>
             <label htmlFor="Age">Age</label>
             <input type="number" id="age" />
             <Button type="submit">Add user</Button>
