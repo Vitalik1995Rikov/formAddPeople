2a1f3d2ccc50d3c4ba6760291f36efa4892c4ea8
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-11 01:25:49 +0300

    added onChange for Age

--- my-form/src/components/Users/AddUser.js
+++ my-form/src/components/Users/AddUser.js
@@ -7,6 +7,7 @@ import classes from './AddUser.module.css';
 const AddUser = props => {
 
     const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     function addUserHandler(event) {
         event.preventDefault();
@@ -16,13 +17,17 @@ const AddUser = props => {
         setEnteredUserName(event.target.value);
     }
 
+    function ageChangeHandler(event) {
+        setEnteredAge(event.target.value);
+    }
+
     return (
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input type="text" id="username" onChange={userNameChangeHandler}/>
             <label htmlFor="Age">Age</label>
-            <input type="number" id="age" />
+            <input type="number" id="age" onChange={ageChangeHandler}/>
             <Button type="submit">Add user</Button>
         </form>
     </Card>
