4ded6781250df0248ae24453239d8bde9558b6d0
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-13 12:07:03 +0300

    added styles for ErrorModal

--- my-form/src/components/UI/ErrorModal.js
+++ my-form/src/components/UI/ErrorModal.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = props => {
     return (
         <Card>
             <header>
-                <h2></h2>
+                <h2>{props.title}</h2>
             </header>
             <div>
-                <p></p>
+                <p>{props.message}</p>
             </div>
             <footer>
-
+                <Button>Okay</Button>
             </footer>
         </Card>
     )
