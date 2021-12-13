d31c0057a88645a6e55f334a6186e2fb1a605d51
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-13 11:26:44 +0300

    added hook useState for userList in App

--- my-form/src/App.js
+++ my-form/src/App.js
@@ -1,8 +1,13 @@
+import React, { useState } from "react";
+
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 
 function App() {
+
+  const [userList, setUserList] = useState([]);
+
   return (
     <div>
       <AddUser/>
