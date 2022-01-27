12a100d1dcc081a1e305b33c935cfc1163ce6407
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-27 21:45:07 +0300

    change the component Check

--- my-form/src/components/screens/Home/item/Check.js
+++ my-form/src/components/screens/Home/item/Check.js
@@ -3,7 +3,7 @@ import { BsCheck } from 'react-icons/bs';
 
 const Check = ({isCompleted}) => {
     return (
-        <div className="border-2 rounded-lg border-pink-400 w-5 h-5 mr-3">
+        <div className={`border-2 rounded-lg border-pink-400 ${isCompleted ? 'bg-pink-400' : ''} w-5 h-5 mr-3`}>
             {isCompleted ? <BsCheck size={24} className="text-gray-900"/> : null}
         </div>
             
