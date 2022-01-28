8d4072835ace5fc36e61b4dd92c1bbb0f5f10dd2
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-28 11:50:41 +0300

    added components Blog, Agents, OurProperty

---
+++ my-form/src/components/screens/Agents.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const data = [
+    {
+        id: 1,
+        name: 'Alex',
+        surname: 'Black',
+        position: 'manager'
+    },
+    {
+        id: 2,
+        name: 'Evgeny',
+        surname: 'White',
+        position: 'developer'
+    },
+    {
+        id: 3,
+        name: 'Jim',
+        surname: 'Crosby',
+        position: 'engirneer'
+    }
+]
+
+const Agents = () => {
+    return (
+        <div></div>
+    )
+}
---
+++ my-form/src/components/screens/OurProperty.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OurProperty = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default OurProperty
