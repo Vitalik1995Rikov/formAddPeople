708c5f8a72ac3bfe7f4e5166c1b83ff24f7952d2
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-28 15:19:10 +0300

    some changes in Description component

--- my-form/src/components/items/Description/Description.js
+++ my-form/src/components/items/Description/Description.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Description = () => {
     return (
-        <div>
-            <h1>Let's Discus With Us About Your House</h1>
-            <h2>Contact us</h2>
-            <span>We are a property consultion company. We have operating 20 years to help families</span>
+        <div className="flex">
+            <div className="">
+                <h1 className="text-8xl max-w-2xl">Let's Discus With Us About Your House.</h1>
+                <h2 className="text-lg max-w-2xl">Contact us</h2>
+                <span className="text-md max-w-2xl">We are a property consultion company. We have operating 20 years to help families</span>
+            </div>
+            <div>
+                <img src="" alt="img" />
+            </div>
         </div>
+        
         
     )
 }
