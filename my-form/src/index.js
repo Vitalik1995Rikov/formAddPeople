9899e18a1defa02a948ae9cee664d7ef7c8d0f90
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-28 12:19:42 +0300

    added NotFound component

--- my-form/src/index.js
+++ my-form/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import './index.css';
 
@@ -8,16 +8,18 @@ import Home from './components/screens/Home';
 import Agents from './components/screens/Agents';
 import Blog from './components/screens/Blog';
 import OurProperty from './components/screens/OurProperty';
+import NotFound from './components/screens/NotFound';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Home/>
-        <Agents/>
-        <Blog/>
-        <OurProperty/>
-      </BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/agents" element={<Agents/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/property" element={<OurProperty/>}/>
+        <Route path="/*" element={<NotFound/>}/>
+    </Routes>
   </React.StrictMode>,
   document.getElementById('root')
 );
