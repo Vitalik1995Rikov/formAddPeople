0584db6eb43ae05e0cd3e492dd76b7832827d684
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-28 11:18:48 +0300

    clean project

--- my-form/src/index.js
+++ my-form/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './components/screens/Home/Home';
-import Layout from './components/layout/Layout'
+import Home from './components/screens/Home';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout>
       <Home/>
-    </Layout>
   </React.StrictMode>,
   document.getElementById('root')
 );
