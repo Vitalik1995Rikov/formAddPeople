3a817c88e54b65c660361c8f4d4d423f249aa3a6
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-28 11:25:04 +0300

    added component Header

---
+++ my-form/src/components/items/Header.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Solary</h1>
+            <h2>Home</h2>
+            <h2>Our property</h2>
+            <h2>Agents</h2>
+            <h2>Blog</h2>
+            <button>Sign up</button>
+        </div>
+    )
+}
+
+export default Header
